5a5aa45d12830ce00e53645dc88151206b685b9a
Muhammad-Usama-1 <mdusama225@gmail.com>
2021-06-21 09:22:24 +0500

    f change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Fundamentals
+          Learn Fundamental
         </a>
       </header>
     </div>
